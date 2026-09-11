6c8fcdb8c4d8f3a5d1ce996165c32c3463e130b8
Thomas Vu <thomasjvu@gmail.com>
2026-09-11 17:59:52 -0400

    Sync @phantasy/plugin-twilio (source-extracted) from monorepo export

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export declare class TwilioPlugin {
+  name: string;
+  version: string;
+  description: string;
+  getTools(): never[];
+}
+export default TwilioPlugin;
---
+++ dist/index.js
@@ -0,0 +1,9 @@
+export class TwilioPlugin {
+  name = "twilio";
+  version = "0.1.17-beta";
+  description = "Twilio SMS and MMS messaging integration plugin for Phantasy companions.";
+  getTools() {
+    return [];
+  }
+}
+export default TwilioPlugin;
